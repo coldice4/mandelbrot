791a62d1dce3e61da9b3eb7c7aa9357687e2cad2
Stephan Eder <stephan@unicornheaven.net>
2019-11-14 22:32:50 +0100

    second commit

--- main.go
+++ main.go
@@ -19,87 +19,79 @@ type datapoint struct {
 }
 
 var waitGroup sync.WaitGroup
-var db sql.DB
+var db *sql.DB
 
 func main() {
 	fmt.Printf("%s\n", "Mandelbrot Generator")
 
-	db, err := sql.Open("mysql", "mandelbrot:password@tcp(127.0.0.1:3306)/mandelbrot")
+	var err error
+	db, err = sql.Open("mysql", "mandelbrot:password@tcp(127.0.0.1:3306)/mandelbrot")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	//stmt, err := db.Prepare("INSERT INTO mandelbrot(x, y, iterations) VALUES (?,?,?)")
 
+	var px_x = 1920*10
+	var px_y = 1080*10
 
-	var px_x = 1920*2
-	var px_y = 1080*2
-
-	var max_iteration = 1000
+	//var max_iteration = 16777215 //max max_iterations value the db can handle
+	var max_iteration = 10000
 
 	var mutex = &sync.Mutex{}
-
-	//image := image2.NewNRGBA(image2.Rectangle{image2.Point{0,0}, image2.Point{px_x, px_y}})
+	throttle := make(chan struct{}, 32)
 
 	for px_xi := 0; px_xi < px_x; px_xi++ {
 		waitGroup.Add(1)
+		throttle <- struct{}{}
+
 		go func(px_xi int) {
 			defer waitGroup.Done()
+			defer func() {<-throttle}()
 
 			var rows []datapoint
 			for px_yi := 0; px_yi < px_y; px_yi++ {
-				//x0 := float64(px_xi) / float64(px_x) * 3.5 -2.5
-				//x0 := scale(px_xi, 0, px_x, -2.5, 1)
-				x0 := scale(px_xi, 0, px_x, -0.035, 0.025)
-				//y0 := float64(px_yi) / float64(px_y) * 2 - 1
-				//y0 := scale(px_yi, 0, px_y, -1, 1)
-				y0 := scale(px_yi, 0, px_y, 0.73, 0.77)
+				x0 := scale(px_xi, 0, px_x, -2.5, 1)
+				//x0 := scale(px_xi, 0, px_x, -0.035, 0.025)
+				y0 := scale(px_yi, 0, px_y, -1, 1)
+				//y0 := scale(px_yi, 0, px_y, 0.73, 0.77)
 
 				iterations := calc_iterations(x0, y0, max_iteration)
 
-				dp := datapoint{x: px_xi, y: px_yi, iterations: iterations}
-				rows = append(rows, dp)
+				if iterations != max_iteration {
+					dp := datapoint{x: px_xi, y: px_yi, iterations: iterations}
+					rows = append(rows, dp)
+				}
 
-				if (len(rows) >= 1000) {
+				if (len(rows) >= 10000) {
 					mutex.Lock()
+					fmt.Printf("Starting insert for row: %d\n", px_xi)
 					err = bulkInsert(rows)
 					if err != nil {
 						fmt.Printf("%s\n", err)
 					}
 					rows = nil
+					fmt.Printf("Finished insert\n")
 					mutex.Unlock()
 				}
+			}
 
-				/*_, err := stmt.Exec(px_xi, px_yi, iterations)
+			if rows != nil {
+				mutex.Lock()
+				fmt.Printf("Starting insert for row: %d\n", px_xi)
+				err = bulkInsert(rows)
 				if err != nil {
 					fmt.Printf("%s\n", err)
-				}*/
-
-
-
-
-				/*iteration_scaled := uint8(scale(iteration, 0, max_iteration, 0, 255))
-
-				if iteration < 1000 {
-					image.Set(px_xi, px_yi, color.RGBA{R:iteration_scaled, G:0, B: 0, A: 255})
 				}
-				if (iteration < 666) {
-					image.Set(px_xi, px_yi, color.RGBA{R:iteration_scaled, G:iteration_scaled, B: 0, A: 255})
-				}
-				if (iteration < 333) {
-					image.Set(px_xi, px_yi, color.RGBA{R:iteration_scaled, G:iteration_scaled, B: iteration_scaled, A: 255})
-				}*/
-
+				rows = nil
+				fmt.Printf("Finished insert\n")
+				mutex.Unlock()
 			}
 		}(px_xi)
 		fmt.Printf("Finished Column: %d\n", px_xi+1)
 	}
 
 	waitGroup.Wait()
-
-	//output, _ := os.Create("mandelbrot.png")
-	//png.Encode(output, image)
 }
 
 func bulkInsert(rows []datapoint) (err error) {
@@ -114,10 +106,6 @@ func bulkInsert(rows []datapoint) (err error) {
 	}
 
 	stmt := fmt.Sprintf("INSERT INTO mandelbrot (x, y, iterations) VALUES %s", strings.Join(valueStrings, ","))
-	fmt.Printf("%d\n", len(rows))
-	fmt.Printf("%d\n", len(valueArgs))
-	fmt.Printf("%+v\n", valueArgs)
-	fmt.Printf("%+v\n", valueStrings)
 	_, err = db.Exec(stmt, valueArgs...)
 	return err
 }
